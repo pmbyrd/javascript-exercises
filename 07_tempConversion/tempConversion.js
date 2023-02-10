3fdfc76884344149f0694b9d446c76da5eb50f74
Perriesha <pmbyrd@gmail.com>
2023-02-09 19:46:54 -0800

    tempConcersion Test passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+// write a function that converts a temperature from Fahrenheit to Celsius
+// T(°C) = (T(°F) - 32) × 5/9
+// round to 1 decimal
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * (5/9);
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// write a function that converts a temperature from Celsius to Fahrenheit
+// T(°F) = T(°C) × 9/5 + 32
+const convertToFahrenheit = function(temp) {
+  temp = (temp * (9/5)) + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
