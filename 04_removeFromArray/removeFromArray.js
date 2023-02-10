8d5ff009b93fc46d2b408bdf939f6e16623f2216
Perriesha <pmbyrd@gmail.com>
2023-02-09 19:17:46 -0800

    Removal & Test passing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
+// param arr, ...args
+// loop through the array and check if the value is in the array
+// if it is, remove it
+// return arr
+console.log("hello world")
+console.log("hello again")
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    let args = Array.from(arguments).slice(1);
+    for (let i = 0; i < args.length; i++) {
+        if (arr.includes(args[i])) {
+            arr.splice(arr.indexOf(args[i]), 1);
+        }
+    }
+    return arr;
 };
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
