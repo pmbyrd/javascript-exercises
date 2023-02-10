83f8dd3aadafa5e945a2704bcbdb53e700c519fb
Perriesha <pmbyrd@gmail.com>
2023-02-09 18:47:57 -0800

    01,02,03 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+   let result = '';
+    for (let i = 0; i < number; i++) {
+        result += string;
+    } if (number < 0) {
+        return 'ERROR';
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+// a string passed in as an argument
+// split the string into an array of characters
+// reverse the array
+// join the array back into a string
+// a series of steps that can be chained together
 
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
