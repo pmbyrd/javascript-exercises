c590308db393c1d4c9930c9b7a25343521677169
Perriesha <pmbyrd@gmail.com>
2023-02-15 09:55:16 -0800

    Failed test on to be false

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const strArr = str.toLowerCase().split("")
+  const strArrReveresed = strArr.reverse()
+  if (strArr.join("") === strArrReveresed.join("")) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
