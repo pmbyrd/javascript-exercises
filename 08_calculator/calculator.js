95d1ff1577408088f5cc25f636ba0b01af467ae4
Perriesha <pmbyrd@gmail.com>
2023-02-15 09:34:03 -0800

    Calculator test passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// reduce method
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) { // 0! = 1
+    return 1;
+  } else {
+    return n * factorial(n - 1); // call each time with n - 1 until n = 0
+  }
 };
 
 // Do not edit below this line
