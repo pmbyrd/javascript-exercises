5037939d587c4749c2ea1681825e73bc75ee5f26
Perriesha <pmbyrd@gmail.com>
2023-02-15 10:27:30 -0800

    Completed until exercise 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // no negative numbers
+    // can be a string
+    // a fibonacci sequence is a sequence of numbers where the next number is the sum of the previous two numbers
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    } else if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+ 
+// # Exercise 12 - Find the Oldest
 
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+
+// ## Hints
+// - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
+// - this can be done with a couple of chained array methods, or by using `reduce`.
+// - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
+//
+const findTheOldest = function(person) {
+    const oldest = person.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+    return oldest;
 };
 
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+console.log("hello world")
 // Do not edit below this line
 module.exports = findTheOldest;
