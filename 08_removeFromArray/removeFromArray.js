a82932173cb64983ffd33f90398fec0812c1bd74
pmbyrd <pmbyrd@gmail.com>
2025-06-07 16:22:14 -0700

    feat: 08 funtion has been implemented all tests are passing

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+
+const removeFromArray = function (array, ...elements) {
+    let index
+    for (let el of elements) {
+        //change the conditional to continue to loop even if the element if found or else it will exit the loop having once met the condition
+        while (array.includes(el)) {
+            index = array.indexOf(el)
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
