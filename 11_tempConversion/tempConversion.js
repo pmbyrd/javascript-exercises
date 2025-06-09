288466cfac6a94cd89f4f9f57b664f189918b008
pmbyrd <pmbyrd@gmail.com>
2025-06-08 20:45:57 -0700

    feat: 11 functions implemented and test passing

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  //C=(F-32) * 5/9
+  const C = Number((temp - 32) * 5 / 9)
+  return C % 1 === 0 ? C : Number(C.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  //F=(9/5 * C) + 32
+  const F = Number((9/5 * temp) + 32)
+  return F % 1 === 0 ? F : Number(F.toFixed(1)) 
 };
 
+convertToCelsius(32)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
