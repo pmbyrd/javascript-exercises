71aad18090836d7a5ed353b98d95075d76a0a24e
pmbyrd <pmbyrd@gmail.com>
2025-06-15 21:17:48 -0700

    feat: palindrome itialized not all test passing hedge cases are not passing

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let result = phrase.toLowerCase().split("").toReversed().join("")
+    console.log(result)
 
+    if(phrase === result) return true
 };
+let result = palindromes("racecar")
+result = palindromes("hello")
 
 // Do not edit below this line
 module.exports = palindromes;
