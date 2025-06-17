37174e334761d8cf43d8b8c650fba17756323177
pmbyrd <pmbyrd@gmail.com>
2025-06-16 20:42:05 -0700

    feat: finding oldest tests are not passing yet manual input is getting the oldest for all the test cases

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
+  console.log(currentYear);
+  let modifiedArray = [];
+  modifiedArray = array
+    .map((item) => {
+      const yearOfDeath = Object.hasOwn(item, "yearOfDeath")
+        ? item.yearOfDeath
+        : currentYear;
+      const yearsLived = yearOfDeath - item.yearOfBirth;
+      return { ...item, yearOfDeath: currentYear, yearsLived };
+    })
+    .reduce((max, current) => {
+        return current.yearsLived > max.yearsLived ? current : max
+    });
+    console.log(modifiedArray.name)
+  return modifiedArray.name;
 };
-
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
