ce78d110a87376b4a3932730abecb32b770f042e
pmbyrd <pmbyrd@gmail.com>
2025-06-16 19:33:37 -0700

    feat: fibonacci intialized

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let fib;
+  //account for user errors
+  num = parseInt(num)
+  if (num < 0) return "OOPS"
+  if (num === 0) return 0;
+  if (num === 1 || num === 2) return 1;
+  //calls the function recussively to get the values
+  fib = fibonacci(num - 1) + fibonacci(num - 2);
+  return fib
 };
 
 // Do not edit below this line
